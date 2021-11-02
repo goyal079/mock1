22e0f10063a9c83465a6a3b19fbd36ece5821daf
goyal <goyalhimanshu079@gmail.com>
2021-11-02 15:03:47 +0530

    mock

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
-console.log("hello world Git & Github")
-console.log("HELLO WORLD")
-
+console.log("hello world Git & Github");
+console.log("HELLO WORLD");
+var a = [1, 2, 3, 4, 5, 6, 7];
+console.log(a);
